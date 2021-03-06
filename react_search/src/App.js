54eddda2e4e7216fa4099694ff647b17f55a8823
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-05 23:10:25 -0500

    search button issue solved

--- react_search/src/App.js
+++ react_search/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       people: [],
+      searchField: "",
     };
   }
 
@@ -20,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="Seach People" onChange={e => this.setState({searchField:e.target.value})} />
         <CardList people={this.state.people}></CardList>
       </div>
     );
