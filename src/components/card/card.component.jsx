1a81632fa97a3cdd32ac28f58a103e8a5b189b4c
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-06 12:01:49 -0500

    img alt updates

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,11 @@ import "./card.style.css";
 
 export const Card = (props) => (
   <div className="card-container">
-  <img className="img" alt="people" src={`../images/${props.people.id}.png`}/>
+    <img
+      className="img"
+      alt="people"
+      src={`../images/${props.people.id}.png`}
+    />
     <h2>{props.people.name}</h2>
     <p>{props.people.email}</p>
   </div>
